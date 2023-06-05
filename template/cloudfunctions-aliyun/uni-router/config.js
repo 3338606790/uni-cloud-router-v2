ec46a08d83a68229a23474d814e31028dd55ea78
Admin <3338696790@qq.com>
2023-06-05 17:10:29 +0800

    init

---
+++ template/cloudfunctions-aliyun/uni-router/config.js
@@ -0,0 +1,11 @@
+const logger = require('./middleware/logger.js')
+module.exports = {
+    debug: true, // 输出调试信息
+    baseDir: __dirname, // 应用根目录
+    middleware: [ // 中间件
+        [logger()]
+    ],
+    routes: {
+        "/test/home/index": "home.index",
+    }
+}
